069498c446787002bcfd43d565f400e2dfd439ab
matthew <g00430349@atu.ie>
2024-10-20 20:42:57 +0100

    Added try catch aswell as added while loop allowing to repeat until correct value was entered

--- src/main/java/atu/ie/TryCatch.java
+++ src/main/java/atu/ie/TryCatch.java
@@ -5,15 +5,24 @@ import java.util.Scanner;
 public class TryCatch {
     public static void main(String[] args) {
         System.out.println("please enter a number between 0 and 9");
-        Scanner myscan = new Scanner(System.in);
 
-        String input = myscan.nextLine();
-        int mynum = Integer.parseInt(input);
-        if(mynum >= 0 && mynum <= 9) {
-            System.out.println(mynum);
-        }
-        else {
-            System.out.println("invalid number");
+
+        try (Scanner myscan = new Scanner(System.in)) {
+            int mynum = -1;
+            while (mynum < 0 || mynum > 9) {
+                String input = myscan.nextLine();
+                try {
+                    mynum = Integer.parseInt(input);
+                    if (mynum >= 0 && mynum <= 9) {
+                        System.out.println("the number is " + mynum);
+                    } else {
+                        System.out.println("invalid number");
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("invalid number " + e.getMessage());
+                }
+            }
+
         }
     }
 }
